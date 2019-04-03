0aa7036b205ea8d44a15683ab760e9ee01ca9a4a
Jakub Ruszil <jakubruszil@gmail.com>
2019-04-03 16:21:41 +0200

    Initial Commit

---
+++ bachelor_squares/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  bachelor_squares
+//
+//  Created by Jakub Ruszil on 03/04/2019.
+//  Copyright © 2019 Jakub Ruszil. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

8e4a13589755c270fbfe200d4e600f2631e4a15d
Jakub Ruszil <jakubruszil@gmail.com>
2019-04-04 13:46:26 +0200

    traversals

--- bachelor_squares/main.cpp
+++ bachelor_squares/main.cpp
@@ -3,6 +3,57 @@
 #include <cstdio>
 
 int count = 0;
+
+bool canBeTransversal(char **square, char** result, int n, int k, int val){
+    if(val == k%n){
+        return false;
+    }
+    else{
+        for(int i = 0; i < k; i++){
+            char tmp = result[i/n][i%n];
+            if(tmp == val){
+                if(square[i/n][i%n] == square[k/n][k%n]){
+                    return false;
+                }
+            }
+        }
+    }
+    return true;
+}
+
+
+void checkTransversalsRec(char **square, char** result, int n, int k){
+    if(k == n*n){
+        return;
+    }
+    else{
+        for(int i = 0; i < n; i++){
+            if(canBeTransversal(square,result, n, k, i)){
+                result[k/n][k%n] = i;
+                return checkTransversalsRec(square,result, n, k + 1);
+            }
+        }
+    }
+}
+
+char** checkTransversals(char** square, int n){
+    char** result = new char*[n];
+    for(int i = 0; i < n; i++){
+        result[i] = new char[n];
+    }
+    for(int i = 0; i < n; i++){
+        result[0][i] = i;
+    }
+    for(int i = 1; i < n; i++){
+        for(int j = 0; j < n; j++){
+            result[i][j] = -1;
+        }
+    }
+    checkTransversalsRec(square, result, n, n);
+    return result;
+}
+
+
 bool canExtendToLatinSquare(int n, char** square, int k, int val){
     int column = k%n;
     int row = k/n;
@@ -34,6 +85,7 @@ void printLatinSquare(char** square, int n){
 void generateReducedLatinSquaresRec(int k, char** square, int n){
     int size = n*n;
     if(k == size){
+        printLatinSquare(checkTransversals(square, n), n);
         count++;
     }
     else{
@@ -71,20 +123,5 @@ void generateReducedLatinSquares(int n){
 
 int main()
 {
-    /*char** square = new char*[4];
-    for (int i = 0; i < 4; i++)
-        square[i] = new char[4];
-    
-    for (int i = 0; i < 4; i++){
-        square[0][i] = i;
-        square[i][0] = i;
-    }
-    
-    printLatinSquare(square, 4);
-    
-    printf("%d\n",canExtendToLatinSquare(4, square, 6, 3));
-    
-    */
-    generateReducedLatinSquares(7);
-    printf("%d ", count);
+    generateReducedLatinSquares(6);
 }
